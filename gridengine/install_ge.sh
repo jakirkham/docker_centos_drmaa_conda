e748454c4e58cc04cd37ceb066cbe5e6f629c57f
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-06-01 01:29:06 -0400

    gridengine/install_ge.sh: Expose all relevant environment variables from `settings.sh`.

--- gridengine/install_ge.sh
+++ gridengine/install_ge.sh
@@ -19,6 +19,7 @@ service gridengine-master restart
 service gridengine-exec restart
 export CORES=$(grep -c '^processor' /proc/cpuinfo)
 $SGE_CONFIG_DIR/inst_sge -x -m -auto $SGE_CONFIG_DIR/docker_configuration.conf
+source $SGE_CONFIG_DIR/default/common/settings.sh
 cp $SGE_CONFIG_DIR/user.conf.tmpl $SGE_CONFIG_DIR/user.conf
 sed -i -r "s/template/${USER}/" $SGE_CONFIG_DIR/user.conf
 qconf -suserl | xargs -r -I {} qconf -du {} arusers
