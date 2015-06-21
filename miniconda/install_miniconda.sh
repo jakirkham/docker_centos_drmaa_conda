76e27d4e572f164af5125eba27ace446cdd670f7
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-06-20 23:40:59 -0400

    miniconda/install_miniconda.sh: Always install the latest version of Miniconda.

--- miniconda/install_miniconda.sh
+++ miniconda/install_miniconda.sh
@@ -14,7 +14,7 @@ yum clean all
 
 # Download and configure conda.
 cd /usr/share/miniconda
-curl http://repo.continuum.io/miniconda/Miniconda-3.9.1-Linux-x86_64.sh > miniconda.sh
+curl http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh > miniconda.sh
 bash miniconda.sh -b -p /opt/conda
 export PATH="/opt/conda/bin:${PATH}"
 source activate root
