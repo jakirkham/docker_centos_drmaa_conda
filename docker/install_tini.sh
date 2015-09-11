b21b3c2a449e1f51a5fc1aefb8da12a3a87ccacd
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-09-11 08:52:16 -0400

    docker/install_tini.sh: Get and install the latest version of `tini` from GitHub.

--- docker/install_tini.sh
+++ docker/install_tini.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+
+yum -y install curl grep sed rpm
+TINI_VERSION=`curl https://github.com/krallin/tini/releases/latest | grep -o "/v.*\"" | sed 's:^..\(.*\).$:\1:'`
+rpm -ivh "https://github.com/krallin/tini/releases/download/v${TINI_VERSION}/tini_${TINI_VERSION}.rpm" && \
+yum clean all
