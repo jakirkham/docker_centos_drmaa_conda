17f512690d51a167e919d435ede9d947255d7c5a
John Kirkham <kirkhamj@janelia.hhmi.org>
2016-02-18 00:41:58 -0500

    docker/install_tini.sh: Install tini with `yum` directly.

--- docker/install_tini.sh
+++ docker/install_tini.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 yum -y update -q
-yum -y install curl grep sed rpm
+yum -y install curl grep sed
 TINI_VERSION=`curl https://github.com/krallin/tini/releases/latest | grep -o "/v.*\"" | sed 's:^..\(.*\).$:\1:'`
-rpm -Uvh "https://github.com/krallin/tini/releases/download/v${TINI_VERSION}/tini_${TINI_VERSION}.rpm"
-rpm --rebuilddb
+yum install -y "https://github.com/krallin/tini/releases/download/v${TINI_VERSION}/tini_${TINI_VERSION}.rpm"
 yum clean all
