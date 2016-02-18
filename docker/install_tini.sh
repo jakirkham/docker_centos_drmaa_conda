0b3195b6a654ec1916b5b7756cb51beb5b5573fa
John Kirkham <kirkhamj@janelia.hhmi.org>
2016-02-17 23:59:24 -0500

    docker/install_tini.sh: Change `rpm`'s arguments.

--- docker/install_tini.sh
+++ docker/install_tini.sh
@@ -3,5 +3,5 @@
 yum -y update -q
 yum -y install curl grep sed rpm
 TINI_VERSION=`curl https://github.com/krallin/tini/releases/latest | grep -o "/v.*\"" | sed 's:^..\(.*\).$:\1:'`
-rpm -ivh "https://github.com/krallin/tini/releases/download/v${TINI_VERSION}/tini_${TINI_VERSION}.rpm"
+rpm -Uvh "https://github.com/krallin/tini/releases/download/v${TINI_VERSION}/tini_${TINI_VERSION}.rpm"
 yum clean all
