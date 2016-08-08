0209a4f7488f7ef87dca6e50fbbe12f6a7f68581
John Kirkham <kirkhamj@janelia.hhmi.org>
2016-08-08 12:22:38 -0400

    miniconda: `CONDA_PATH` -> `INSTALL_CONDA_PATH`.
    Appears that `CONDA_PATH` is now being used by `conda` and is being
    unset in some cases. This changes the environment variable we use to
    `INSTALL_CONDA_PATH` so that we don't conflict with `conda`.

--- miniconda/install_miniconda.sh
+++ miniconda/install_miniconda.sh
@@ -22,16 +22,16 @@ yum clean all
 export OLD_PATH="${PATH}"
 for PYTHON_VERSION in 2 3;
 do
-    export CONDA_PATH="/opt/conda${PYTHON_VERSION}"
+    export INSTALL_CONDA_PATH="/opt/conda${PYTHON_VERSION}"
 
     # Download and install `conda`.
     cd /usr/share/miniconda
     curl "http://repo.continuum.io/miniconda/Miniconda${PYTHON_VERSION}-latest-Linux-x86_64.sh" > "miniconda${PYTHON_VERSION}.sh"
-    bash "miniconda${PYTHON_VERSION}.sh" -b -p "${CONDA_PATH}"
+    bash "miniconda${PYTHON_VERSION}.sh" -b -p "${INSTALL_CONDA_PATH}"
     rm "miniconda${PYTHON_VERSION}.sh"
 
     # Configure `conda` and add to the path
-    export PATH="${CONDA_PATH}/bin:${OLD_PATH}"
+    export PATH="${INSTALL_CONDA_PATH}/bin:${OLD_PATH}"
     source activate root
     conda config --set show_channel_urls True
 
@@ -49,9 +49,9 @@ do
     conda clean -yitps
 
     # Provide links in standard path.
-    ln -s "${CONDA_PATH}/bin/python"  "/usr/local/bin/python${PYTHON_VERSION}"
-    ln -s "${CONDA_PATH}/bin/pip"  "/usr/local/bin/pip${PYTHON_VERSION}"
-    ln -s "${CONDA_PATH}/bin/conda"  "/usr/local/bin/conda${PYTHON_VERSION}"
+    ln -s "${INSTALL_CONDA_PATH}/bin/python"  "/usr/local/bin/python${PYTHON_VERSION}"
+    ln -s "${INSTALL_CONDA_PATH}/bin/pip"  "/usr/local/bin/pip${PYTHON_VERSION}"
+    ln -s "${INSTALL_CONDA_PATH}/bin/conda"  "/usr/local/bin/conda${PYTHON_VERSION}"
 done
 
 # Set the conda2 environment as the default.
