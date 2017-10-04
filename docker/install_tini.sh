16b93fb1d6da4cae04fa66cc8a49bc706493b146
John Kirkham <kirkhamj@janelia.hhmi.org>
2017-10-04 13:21:23 -0400

    Drop tini install script

--- docker/install_tini.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-yum update -y -q
-yum install -y -q curl grep sed
-TINI_VERSION=`curl https://github.com/krallin/tini/releases/latest | grep -o "/v.*\"" | sed 's:^..\(.*\).$:\1:'`
-yum install -y -q "https://github.com/krallin/tini/releases/download/v${TINI_VERSION}/tini_${TINI_VERSION}.rpm"
-yum clean all -y -q
